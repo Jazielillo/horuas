c58886e3644c9bdab073aa31937dc55cfec61526
enriqueBueln <chuybuelnaservin@gmail.com>
2025-11-21 18:02:46 -0700

    feat: integrate Prisma Neon adapter and update database connection configuration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,12 @@
 import { PrismaClient } from "@/generated/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import dotenv from "dotenv";
 
-
+dotenv.config();
+const connString = process.env.DATABASE_URL as string;
+const adapter = new PrismaNeon({
+  connectionString: connString,
+});
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -9,7 +15,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query"],
+    adapter,
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
